4ee431b920523facb63d3f4f5101c8049f143aa8
bioa10 <techguy1997@yahoo.com>
2019-03-19 15:10:22 -0500

    Idea should be done for now.

--- Brainstormer/BrainstormerData/Idea.cs
+++ Brainstormer/BrainstormerData/Idea.cs
@@ -22,11 +22,15 @@ namespace BrainstormerData
         /// </summary>
         public Idea(string name, string description, User creator)
         {
+            if (name == null || name == "") throw new ArgumentNullException("Name can not be empty or null.");
+            Name = name;
+            Description = description;
             // if the position values are negative it
             // means that the idea is not displayed
             // in the mindmap
             MindMapX = -1;
             MindMapY = -1;
+            // -1 means no parent
             MindMapParent = -1;
             _votes = 0;
             Creator = creator;
@@ -36,14 +40,41 @@ namespace BrainstormerData
             Image = null;
         }
         /// <summary>
+        /// Sets the name of the idea if the user has permission.
+        /// </summary>
+        /// <param name="name">Accepts a string to set the name to. Can not be empty or null.</param>
+        /// <param name="user">The user that is requesting to set the name.</param>
+        /// <returns>Returns whether the user has permission to set the image. Returns true when successful and false when unsuccessful.</returns>
+        public bool SetName(string name,User user)
+        {
+            if (name == null || name == "") throw new ArgumentNullException("Name can not be empty or null.");
+            if (user == null) throw new ArgumentNullException("user can not be null.");
+            if (user.Equals(Creator)) Name = name;
+            else return false;
+            return true;
+        }
+        /// <summary>
+        /// Sets the description of the idea if the user has permission.
+        /// </summary>
+        /// <param name="description">Accepts a string to set the description to.</param>
+        /// <param name="user">The user that is requesting to set the name.</param>
+        /// <returns>Returns whether the user has permission to set the image. Returns true when successful and false when unsuccessful.</returns>
+        public bool SetDescription(string description, User user)
+        {
+            if (user == null) throw new ArgumentNullException("user can not be null.");
+            if (user.Equals(Creator)) Description = description;
+            else return false;
+            return true;
+        }
+        /// <summary>
         /// Sets the X and Y position of the idea in the MindMap.
         /// </summary>
         /// <param name="X">An int that represents the X position. Must be greater than 0.</param>
         /// <param name="Y">An int that represents the Y position. Must be greater than 0.</param>
         public void SetMindMapPosition(int x, int y)
         {
-            if (x < 0) throw new ArgumentOutOfRangeException("X must be greater than 0.");
-            if (y < 0) throw new ArgumentOutOfRangeException("Y must be greater than 0.");
+            if (x < 0) throw new ArgumentOutOfRangeException("X must be greater than or equal to 0.");
+            if (y < 0) throw new ArgumentOutOfRangeException("Y must be greater than or equal to 0.");
             MindMapX = x;
             MindMapY = y;
         }
@@ -70,16 +101,18 @@ namespace BrainstormerData
         /// </summary>
         /// <param name="image">The image to set on the idea. Pass null to remove the image.</param>
         /// <param name="user">The user that is requesting to set the image.</param>
-        public void SetImage(Image image,User user)
+        /// <returns>Returns whether the user has permission to set the image. Returns true when successful and false when unsuccessful.</returns>
+        public bool SetImage(Image image,User user)
         {
             if (user == null) throw new ArgumentNullException("user can not be null.");
             if (user.Equals(Creator)) Image = image;
-            else throw new ArgumentException("user does not have permission to change image.");
+            else return false;
+            return true;
         }
 
         // ----- default properties -----
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
         public int MindMapX { get; private set; }
         public int MindMapY { get; private set; }
         public List<Comment> Comments { get; private set; }
@@ -94,7 +127,7 @@ namespace BrainstormerData
             get { return _mindMapParent; }
             set
             {
-                if (value < 0) throw new ArgumentOutOfRangeException();
+                if (value < 0) throw new ArgumentOutOfRangeException("ParentID must be greater than or equal to 0.");
                 _mindMapParent = value;
             }
         }
@@ -104,7 +137,7 @@ namespace BrainstormerData
             get { return _votes; }
             set
             {
-                if (value < 0) throw new ArgumentOutOfRangeException();
+                if (value < 0) throw new ArgumentOutOfRangeException("Votes must be greater than or equal to 0.");
                 _votes = value;
             }
         }
